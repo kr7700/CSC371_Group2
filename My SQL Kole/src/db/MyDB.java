2251078616cb1f8fccd90dc23a78bc3ccb4a1138
Unknown <jk1387@ship.edu>
2019-04-18 19:52:41 -0400

    Added code that creates all tables, inserts one row to each, and has functionality to drop all tables as well. Also added three text files in respect to these and removed excess text files.

--- My SQL Kole/src/db/MyDB.java
+++ My SQL Kole/src/db/MyDB.java
@@ -1,11 +1,12 @@
 package db;
 
-import java.io.PrintWriter;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  * A program that creates, inserts to, and selects from a large table in a given
@@ -22,9 +23,9 @@ public class MyDB {
 	/**
 	 * My assigned DB.
 	 */
-	public static final String DB_LOCATION = "jdbc:mysql://db.cs.ship.edu:3306/csc371_17";
-	public static final String LOGIN_NAME = "csc371_17";
-	public static final String PASSWORD = "Password17";
+	public static final String DB_LOCATION = "jdbc:mysql://db.cs.ship.edu:3306/csc371_30";
+	public static final String LOGIN_NAME = "csc371_30";
+	public static final String PASSWORD = "Password30";
 	// Make sure and use the java.sql imports.
 	protected Connection m_dbConn = null;
 
@@ -49,62 +50,41 @@ public class MyDB {
 	public MyDB() throws Exception {
 		activateJDBC(); // activate the DB
 		connectDB(); // connect to the DB
-
-		long startTime; // to calculate time - will convert to milliseconds
-		long endTime;
-		PrintWriter writer; // for writing to output text files
-
-//---------------------------------------------------------------------------(insert block no PK)
-		writer = new PrintWriter("noPK.txt", "UTF-8");
-		for(int i = 1; i <= 10; i++) {
-			createStatementNoPK(); // NO PK
-			startTime = System.nanoTime(); // divide by 1,000,000 to get to milliseconds
-			insertStatementLoop(); // will run 500,000 times each
-			endTime = System.nanoTime();
-			System.out.println(i);
-			writer.println((endTime - startTime)/1000000.0);
-			if(i < 10) { // save the tenth table for the selects
-				dropStatement();
-			}
-		}
-		writer.close();
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------(insert block with PK)
-		writer = new PrintWriter("withPK.txt", "UTF-8");
-		for(int i = 1; i <= 10; i++) {
-			createStatementPK(); // WITH PK
-			startTime = System.nanoTime(); // divide by 1,000,000 to get to milliseconds
-			insertStatementLoop(); // will run 500,000 times each
-			endTime = System.nanoTime();
-			System.out.println(i);
-			writer.println((endTime - startTime)/1000000.0);
-			if(i < 10) { // save the tenth table for the selects
-				dropStatement();
-			}
-		}
-		writer.close();
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------(select block no PK / with PK)
-		writer = new PrintWriter("withPKSelect.txt", "UTF-8");
-		for (int i = 1; i <= 10; i++) {
-			startTime = System.nanoTime(); // divide by 1,000,000 to get to milliseconds
-			selectStatementLoopCol1(i * 100); // will run 100 times each. The i*100 is for an internal loop
-			endTime = System.nanoTime();
-			// System.out.println(i);
-			writer.println((endTime - startTime) / 1000000.0); // to milliseconds
-		}
-		writer.close();
 		
-		writer = new PrintWriter("noPKSelect.txt", "UTF-8");
-		for (int i = 1; i <= 10; i++) {
-			startTime = System.nanoTime(); // divide by 1,000,000 to get to milliseconds
-			selectStatementLoopCol2(i * 100); // will run 100 times each. The i*100 is for an internal loop
-			endTime = System.nanoTime();
-			// System.out.println(i);
-			writer.println((endTime - startTime) / 1000000.0); // to milliseconds
+		File createFile = new File("createTables.txt"); // has all creation statements
+		File insertFile = new File("insertTables.txt"); // has all insert statements
+		File tableNamesReverseOrderFile = new File("tableNamesReverseOrder.txt"); // has names of all tables (in reverse order from creation)
+		Scanner sc = new Scanner(createFile);
+		String line;
+		
+		// creating all tables
+		int tableCount = 1;
+		while(sc.hasNextLine()) {
+			line = sc.nextLine();
+			System.out.println((tableCount++) + " : " + line);
+			
+			createStatementPK(line);
 		}
-		writer.close();
-//---------------------------------------------------------------------------
+		
+		// inserting into all tables
+		sc = new Scanner(insertFile);
+		int insertCount = 1;
+		while(sc.hasNextLine()) {
+			line = sc.nextLine();
+			System.out.println((insertCount++) + " : " + line);
+			
+			insertStatement(line);
+		}
+		
+//		// dropping all tables starting from the most recent table made (reverse order from creation)
+//		sc = new Scanner(tableNamesReverseOrderFile);
+//		int dropCount = 1;
+//		while(sc.hasNextLine()) {
+//			line = sc.nextLine();
+//			System.out.println((dropCount++) + " : " + line);
+//			
+//			dropStatement(line);
+//		}
 	}
 
 	/**
@@ -139,152 +119,191 @@ public class MyDB {
 			sqle.printStackTrace();
 		}
 	}
-
-	/**
-	 * Creates a table with no primary key. To execute an SQL statement that is not
-	 * a SELECT statement. NO PRIMARY KEY.
-	 */
-	public void createStatementNoPK() throws Exception {
-		// Using a PreparedStatement to insert a value (best option when providing
-		// values
-		// from variables).
-		// Use place holders '?' to mark where I am going to provide the data.
-		String insertData = new String(
-				"CREATE TABLE TEST_KOLE (col1 INT,col2 INT,col3 CHAR(10),col4 VARCHAR(30),col5 DOUBLE)");
-		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
-		// When I need to set a primitive type as null.
-		// stmt2.setNull(2, java.sql.Types.INTEGER);
-		int rowsAdded = stmt.executeUpdate();
-		if (rowsAdded == 0) {
-			System.out.println("Created");
-		}
-	}
-
+	
 	/**
 	 * Creates a table with a primary key. To execute an SQL statement that is not a
 	 * SELECT statement. WITH PRIMARY KEY.
 	 */
-	public void createStatementPK() throws Exception {
-		// Using a PreparedStatement to insert a value (best option when providing
-		// values
-		// from variables).
-		// Use place holders '?' to mark where I am going to provide the data.
-		String insertData = new String(
-				"CREATE TABLE TEST_KOLE (col1 INT,col2 INT,col3 CHAR(10),col4 VARCHAR(30),col5 DOUBLE,"
-						+ "PRIMARY KEY (col1))");
+	public void createStatementPK(String line) throws Exception {
+		String insertData = new String(line);
 		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
-		// When I need to set a primitive type as null.
-		// stmt2.setNull(2, java.sql.Types.INTEGER);
 		int rowsAdded = stmt.executeUpdate();
 		if (rowsAdded == 0) {
 			System.out.println("Created");
 		}
 	}
-
+	
+	/**
+	 * Creates a table with a primary key. To execute an SQL statement that is not a
+	 * SELECT statement. WITH PRIMARY KEY.
+	 */
+	public void insertStatement(String line) throws Exception {
+		String insertData = new String(line);
+		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
+		int rowsAdded = stmt.executeUpdate();
+		if (rowsAdded == 1) {
+			System.out.println("Added");
+		}
+	}
+	
 	/**
 	 * Drops the table. To execute an SQL statement that is not a SELECT statement.
 	 * WITH PRIMARY KEY.
 	 */
-	public void dropStatement() throws Exception {
-		// Using a PreparedStatement to insert a value (best option when providing
-		// values
-		// from variables).
-		// Use place holders '?' to mark where I am going to provide the data.
-		String insertData = new String("DROP TABLE TEST_KOLE");
+	public void dropStatement(String line) throws Exception {
+		String insertData = new String("DROP TABLE IF EXISTS " + line);
 		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
-		// When I need to set a primitive type as null.
-		// stmt2.setNull(2, java.sql.Types.INTEGER);
 		int rowsAdded = stmt.executeUpdate();
 		if (rowsAdded == 0) {
 			System.out.println("Dropped");
 		}
 	}
 
-	/**
-	 * Inserts to the table. To execute an SQL statement that is not a SELECT
-	 * statement. WITH PRIMARY KEY.
-	 */
-	public void insertStatementLoop() throws Exception {
-		// Using a PreparedStatement to insert a value (best option when providing
-		// values
-		// from variables).
-		// Use place holders '?' to mark where I am going to provide the data.
-		int percent = 0;
-		String insertData = new String("INSERT INTO TEST_KOLE VALUES (?,?,?,?,?)");
-		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
-		for (int j = 1; j <= 5000; j++) {
-			for (int i = (j * 100 - 99); i <= j * 100; i++) {
-				stmt.setInt(1, i);
-				stmt.setInt(2, i + 1);
-				stmt.setString(3, "a" + i);
-				stmt.setString(4, "a" + (i + 1));
-				stmt.setDouble(5, (double) i);
-				// When I need to set a primitive type as null.
-				// stmt2.setNull(2, java.sql.Types.INTEGER);
-				stmt.addBatch();
-			}
-			stmt.executeBatch();
-			stmt.clearBatch();
-
-			if (j % 500 == 0) { // percentage checkpoints as the table is generated (by 10% chunks)
-				percent += 10;
-				System.out.print("\t" + percent + "%");
-			}
-		}
-		System.out.println("");
-	}
-
-	/**
-	 * Selects from the table and prints out the result. To execute an SQL statement
-	 * that is a SELECT statement.
-	 * 
-	 * @throws SQLException
-	 */
-	public void selectStatementLoopCol1(int j) throws SQLException {
-		String selectData1 = new String("SELECT * FROM TEST_KOLE WHERE col1=?");
-
-		PreparedStatement stmt1 = m_dbConn.prepareStatement(selectData1);
-
-		for (int i = j - 99; i <= j; i++) {
-			stmt1.setInt(1, i);
-
-			ResultSet rs1 = stmt1.executeQuery();
-
-			while (rs1.next()) {
-				// You can access values from a ResultSet either by column number - not advised:
-//				String data = rs1.getString(1);
-//				System.out.print(data + " : ");
-				// Or by column name - advised:
-				String data = rs1.getString("col1");
-				System.out.println(data);
-			}
-		}
-	}
+//	/**
+//	 * Creates a table with no primary key. To execute an SQL statement that is not
+//	 * a SELECT statement. NO PRIMARY KEY.
+//	 */
+//	public void createStatementNoPK() throws Exception {
+//		// Using a PreparedStatement to insert a value (best option when providing
+//		// values
+//		// from variables).
+//		// Use place holders '?' to mark where I am going to provide the data.
+//		String insertData = new String(
+//				"CREATE TABLE TEST_KOLE (col1 INT,col2 INT,col3 CHAR(10),col4 VARCHAR(30),col5 DOUBLE)");
+//		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
+//		// When I need to set a primitive type as null.
+//		// stmt2.setNull(2, java.sql.Types.INTEGER);
+//		int rowsAdded = stmt.executeUpdate();
+//		if (rowsAdded == 0) {
+//			System.out.println("Created");
+//		}
+//	}
 	
-	/**
-	 * Selects from the table and prints out the result. To execute an SQL statement
-	 * that is a SELECT statement.
-	 * 
-	 * @throws SQLException
-	 */
-	public void selectStatementLoopCol2(int j) throws SQLException {
-		String selectData2 = new String("SELECT * FROM TEST_KOLE WHERE col2=?");
+//	/**
+//	 * Creates a table with a primary key. To execute an SQL statement that is not a
+//	 * SELECT statement. WITH PRIMARY KEY.
+//	 */
+//	public void createStatementPK() throws Exception {
+//		// Using a PreparedStatement to insert a value (best option when providing
+//		// values
+//		// from variables).
+//		// Use place holders '?' to mark where I am going to provide the data.
+//		String insertData = new String(
+//				"CREATE TABLE TEST_KOLE (col1 INT,col2 INT,col3 CHAR(10),col4 VARCHAR(30),col5 DOUBLE,"
+//						+ "PRIMARY KEY (col1))");
+//		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
+//		// When I need to set a primitive type as null.
+//		// stmt2.setNull(2, java.sql.Types.INTEGER);
+//		int rowsAdded = stmt.executeUpdate();
+//		if (rowsAdded == 0) {
+//			System.out.println("Created");
+//		}
+//	}
 
-		PreparedStatement stmt2 = m_dbConn.prepareStatement(selectData2);
+//	/**
+//	 * Drops the table. To execute an SQL statement that is not a SELECT statement.
+//	 * WITH PRIMARY KEY.
+//	 */
+//	public void dropStatement() throws Exception {
+//		// Using a PreparedStatement to insert a value (best option when providing
+//		// values
+//		// from variables).
+//		// Use place holders '?' to mark where I am going to provide the data.
+//		String insertData = new String("DROP TABLE TEST_KOLE");
+//		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
+//		// When I need to set a primitive type as null.
+//		// stmt2.setNull(2, java.sql.Types.INTEGER);
+//		int rowsAdded = stmt.executeUpdate();
+//		if (rowsAdded == 0) {
+//			System.out.println("Dropped");
+//		}
+//	}
+	
+//	/**
+//	 * Inserts to the table. To execute an SQL statement that is not a SELECT
+//	 * statement. WITH PRIMARY KEY.
+//	 */
+//	public void insertStatementLoop() throws Exception {
+//		// Using a PreparedStatement to insert a value (best option when providing
+//		// values
+//		// from variables).
+//		// Use place holders '?' to mark where I am going to provide the data.
+//		int percent = 0;
+//		String insertData = new String("INSERT INTO TEST_KOLE VALUES (?,?,?,?,?)");
+//		PreparedStatement stmt = m_dbConn.prepareStatement(insertData);
+//		for (int j = 1; j <= 5000; j++) {
+//			for (int i = (j * 100 - 99); i <= j * 100; i++) {
+//				stmt.setInt(1, i);
+//				stmt.setInt(2, i + 1);
+//				stmt.setString(3, "a" + i);
+//				stmt.setString(4, "a" + (i + 1));
+//				stmt.setDouble(5, (double) i);
+//				// When I need to set a primitive type as null.
+//				// stmt2.setNull(2, java.sql.Types.INTEGER);
+//				stmt.addBatch();
+//			}
+//			stmt.executeBatch();
+//			stmt.clearBatch();
+//
+//			if (j % 500 == 0) { // percentage checkpoints as the table is generated (by 10% chunks)
+//				percent += 10;
+//				System.out.print("\t" + percent + "%");
+//			}
+//		}
+//		System.out.println("");
+//	}
 
-		for (int i = j - 99; i <= j; i++) {
-			stmt2.setInt(1, i + 1);
-
-			ResultSet rs2 = stmt2.executeQuery();
-
-			while (rs2.next()) {
-				// You can access values from a ResultSet either by column number - not advised:
-//				String data = rs1.getString(1);
-//				System.out.print(data + " : ");
-				// Or by column name - advised:
-				String data = rs2.getString("col2");
-				System.out.println(data);
-			}
-		}
-	}
+//	/**
+//	 * Selects from the table and prints out the result. To execute an SQL statement
+//	 * that is a SELECT statement.
+//	 * 
+//	 * @throws SQLException
+//	 */
+//	public void selectStatementLoopCol1(int j) throws SQLException {
+//		String selectData1 = new String("SELECT * FROM TEST_KOLE WHERE col1=?");
+//
+//		PreparedStatement stmt1 = m_dbConn.prepareStatement(selectData1);
+//
+//		for (int i = j - 99; i <= j; i++) {
+//			stmt1.setInt(1, i);
+//
+//			ResultSet rs1 = stmt1.executeQuery();
+//
+//			while (rs1.next()) {
+//				// You can access values from a ResultSet either by column number - not advised:
+////				String data = rs1.getString(1);
+////				System.out.print(data + " : ");
+//				// Or by column name - advised:
+//				String data = rs1.getString("col1");
+//				System.out.println(data);
+//			}
+//		}
+//	}
+	
+//	/**
+//	 * Selects from the table and prints out the result. To execute an SQL statement
+//	 * that is a SELECT statement.
+//	 * 
+//	 * @throws SQLException
+//	 */
+//	public void selectStatementLoopCol2(int j) throws SQLException {
+//		String selectData2 = new String("SELECT * FROM TEST_KOLE WHERE col2=?");
+//
+//		PreparedStatement stmt2 = m_dbConn.prepareStatement(selectData2);
+//
+//		for (int i = j - 99; i <= j; i++) {
+//			stmt2.setInt(1, i + 1);
+//
+//			ResultSet rs2 = stmt2.executeQuery();
+//
+//			while (rs2.next()) {
+//				// You can access values from a ResultSet either by column number - not advised:
+////				String data = rs1.getString(1);
+////				System.out.print(data + " : ");
+//				// Or by column name - advised:
+//				String data = rs2.getString("col2");
+//				System.out.println(data);
+//			}
+//		}
+//	}
 }
