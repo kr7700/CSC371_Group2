8faaec44f862527de4c58450deb27b62f6f7fe0e
Jacob Kole <jk1387@ship.edu>
2019-04-25 17:03:43 -0400

    More work on d1.

--- My SQL Kole/src/db/Display1DBStatements.java
+++ My SQL Kole/src/db/Display1DBStatements.java
@@ -1,12 +1,16 @@
 package db;
 
+import java.io.File;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Display1DBStatements {
 	
 	MyDB db;
+	private String playerName;
+	private int money;
 	
 	public Display1DBStatements() {
 		db = null;
@@ -21,6 +25,14 @@ public class Display1DBStatements {
     	}
 	}
 	
+	public String getPlayerName() {
+		return playerName;
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+	
 	/**
 	 * Selects from the table and prints out the result. To execute an SQL statement
 	 * that is a SELECT statement.
@@ -28,18 +40,52 @@ public class Display1DBStatements {
 	 * @throws SQLException
 	 */
 	public void selectPlayerName() throws SQLException {
-		String selectData1 = new String("SELECT * FROM PLAYER");
+		String selectData1 = new String("SELECT Name FROM PLAYER");
 		PreparedStatement stmt1 = db.getConn().prepareStatement(selectData1);
 		
 		ResultSet rs1 = stmt1.executeQuery();
 
-		while (rs1.next()) {
-			// You can access values from a ResultSet either by column number - not advised:
-//			String data = rs1.getString(1);
-//			System.out.print(data + " : ");
-			// Or by column name - advised:
+//		while (rs1.next()) {
+//			// You can access values from a ResultSet either by column number - not advised:
+////			String data = rs1.getString(1);
+////			System.out.print(data + " : ");
+//			// Or by column name - advised:
+//			String data = rs1.getString("Name");
+//			System.out.println(data);
+//		}
+		
+		if(rs1.next()) {
 			String data = rs1.getString("Name");
-			System.out.println(data);
+			playerName = data;
+			System.out.println(playerName);
+		}
+	}
+	
+	/**
+	 * Selects from the table and prints out the result. To execute an SQL statement
+	 * that is a SELECT statement.
+	 * 
+	 * @throws SQLException
+	 */
+	public void selectMoney() throws SQLException {
+		String selectData1 = new String("SELECT Money FROM PLAYER");
+		PreparedStatement stmt1 = db.getConn().prepareStatement(selectData1);
+		
+		ResultSet rs1 = stmt1.executeQuery();
+
+//		while (rs1.next()) {
+//			// You can access values from a ResultSet either by column number - not advised:
+////			String data = rs1.getString(1);
+////			System.out.print(data + " : ");
+//			// Or by column name - advised:
+//			String data = rs1.getString("Name");
+//			System.out.println(data);
+//		}
+		
+		if(rs1.next()) {
+			int data = rs1.getInt("Money");
+			money = data;
+			System.out.println(money);
 		}
 	}
 	
@@ -52,30 +98,17 @@ public class Display1DBStatements {
 		// values
 		// from variables).
 		// Use place holders '?' to mark where I am going to provide the data.
-		int percent = 0;
 		String insertData = new String("INSERT INTO TEST_KOLE VALUES (?,?,?,?,?)");
 		PreparedStatement stmt = db.getConn().prepareStatement(insertData);
-		for (int j = 1; j <= 5000; j++) {
-			for (int i = (j * 100 - 99); i <= j * 100; i++) {
-				stmt.setInt(1, i);
-				stmt.setInt(2, i + 1);
-				stmt.setString(3, "a" + i);
-				stmt.setString(4, "a" + (i + 1));
-				stmt.setDouble(5, (double) i);
-				// When I need to set a primitive type as null.
-				// stmt2.setNull(2, java.sql.Types.INTEGER);
-				stmt.addBatch();
-			}
-			stmt.executeBatch();
-			stmt.clearBatch();
-
-			if (j % 500 == 0) { // percentage checkpoints as the table is generated (by 10% chunks)
-				percent += 10;
-				System.out.print("\t" + percent + "%");
-			}
-		}
-		System.out.println("");
+		
+//		stmt.setInt(1, i);
+//		stmt.setInt(2, i + 1);
+//		stmt.setString(3, "a" + i);
+//		stmt.setString(4, "a" + (i + 1));
+//		stmt.setDouble(5, (double) i);
+		// When I need to set a primitive type as null.
+		// stmt2.setNull(2, java.sql.Types.INTEGER);
+		
 	}
-	
 	
 }
